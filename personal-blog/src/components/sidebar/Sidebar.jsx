7d6394e78333e9e2bc21364e6d8b7f272a655f69
simo <wambuisimon712@gmail.com>
2023-11-02 18:56:29 +0300

    first commit

--- personal-blog/src/components/sidebar/Sidebar.jsx
+++ personal-blog/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,13 @@
 import "./sidebar.scss";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="modal-wrapper">
       <div className="modal">
-        {/* <span className="close" onClick={() => props.setOpen(false)}>
-          X
-        </span> */}
+        <Link to="/blogs">Blogs</Link>
+        <Link to="/videos">Videos</Link>
+        <Link to="/category">Categories</Link>
       </div>
     </div>
   );
