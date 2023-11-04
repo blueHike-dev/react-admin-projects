0993d3c432520182ac49ad0010e6835379ed41af
simo <wambuisimon712@gmail.com>
2023-11-04 13:23:43 +0300

    first commit

---
+++ personal-blog/src/components/slider/Slider.jsx
@@ -0,0 +1,9 @@
+import "./slider.scss";
+import React from "react";
+
+const Slider = () => {
+  const images = ["simo.jpg", "engineer.jpg", "blues.jpg"];
+  return <div>Slider</div>;
+};
+
+export default Slider;
--- personal-blog/src/pages/home/Home.jsx
+++ personal-blog/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Newsletter from "../../components/newsletter/Newsletter";
+import Slider from "../../components/slider/Slider";
 import "./home.scss";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="home">
+      <Slider />
       <Newsletter />
     </div>
   );
