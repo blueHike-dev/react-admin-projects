bcdb67769ca880a8d1b7f74d844fcda076aadab5
simo <wambuisimon712@gmail.com>
2023-11-08 20:58:04 +0300

    first commit

--- personal-blog/src/components/readMore/ReadMore.jsx
+++ personal-blog/src/components/readMore/ReadMore.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./readMore.scss";
 
 const ReadMore = ({ text, maxLength }) => {
   const [isReadMore, setIsReadMore] = useState(false);
@@ -7,12 +8,17 @@ const ReadMore = ({ text, maxLength }) => {
   };
   return (
     <div>
-      <p>{isReadMore ? text : text.slice(0, maxLength)}</p>
-      {text.length > maxLength && (
-        <button onClick={toggleReadMore}>
-          {isReadMore ? "Read Less" : "Read More"}
-        </button>
-      )}
+      <p>
+        {isReadMore ? text : text.slice(0, maxLength)}{" "}
+        {!isReadMore && text.length > maxLength && (
+          <span className="readmore">...</span>
+        )}
+        {text.length > maxLength && (
+          <button onClick={toggleReadMore}>
+            {isReadMore ? "Read Less" : "Read More"}
+          </button>
+        )}
+      </p>
     </div>
   );
 };
--- personal-blog/src/pages/blogs/Blogs.jsx
+++ personal-blog/src/pages/blogs/Blogs.jsx
@@ -7,8 +7,7 @@ const Blogs = () => {
          Welcome to my blogs page.The purpose of the page is to share my passion,
         and that includes Coding, Reading Books and taking Walks. So I will be
         sharing all I have learnt from the books that I read and what benefits I
-        get from taking walks on the daily basis. Also, since Im a self-taught */}
-        {/* developer, I will be sharing all the challenges I faced on my journey of
+        get from taking walks on the daily basis. Also, since Im a self-taught developer, I will be sharing all the challenges I faced on my journey of
         becoming a software developer. I will also be sharing all the errors
         that I encouter on a daily or weekly basis and the solution to them when
         I solve them. Like I said before, my purpose has always been to make the
@@ -18,19 +17,7 @@ const Blogs = () => {
   return (
     <div className="blogs-page">
       <div className="introduction">
-        <ReadMore text={introWords} maxLength={50} />
-
-        {/* Welcome to my blogs page.The purpose of the page is to share my passion,
-        and that includes Coding, Reading Books and taking Walks. So I will be
-        sharing all I have learnt from the books that I read and what benefits I
-        get from taking walks on the daily basis. Also, since Im a self-taught */}
-        {/* developer, I will be sharing all the challenges I faced on my journey of
-        becoming a software developer. I will also be sharing all the errors
-        that I encouter on a daily or weekly basis and the solution to them when
-        I solve them. Like I said before, my purpose has always been to make the
-        world a better place and feel like this is the safiest space to
-        kickstart my journey. I hope you will enjoy everything that I will be
-        sharing. */}
+        <ReadMore text={introWords} maxLength={321} />
       </div>
       <div className="posts"></div>
     </div>
