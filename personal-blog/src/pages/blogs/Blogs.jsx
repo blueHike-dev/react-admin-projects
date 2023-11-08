a48e50bc8eff8fe0ef834dae607b9e2fa7a230a4
simo <wambuisimon712@gmail.com>
2023-11-08 18:38:39 +0300

    first commit

--- personal-blog/src/pages/blogs/Blogs.jsx
+++ personal-blog/src/pages/blogs/Blogs.jsx
@@ -2,7 +2,24 @@ import "./blogs.scss";
 import React from "react";
 
 const Blogs = () => {
-  return <div className="blogs">blogs</div>;
+  return (
+    <div className="blogs-page">
+      <div className="introduction">
+        Welcome to my blogs page.The purpose of the page is to share my passion,
+        and that includes Coding, Reading Books and taking Walks. So I will be
+        sharing all I have learnt from the books that I read and what benefits I
+        get from taking walks on the daily basis. Also, since Im a self-taught
+        developer, I will be sharing all the challenges I faced on my journey of
+        becoming a software developer. I will also be sharing all the errors
+        that I encouter on a daily or weekly basis and the solution to them when
+        I solve them. Like I said before, my purpose has always been to make the
+        world a better place and feel like this is the safiest space to
+        kickstart my journey. I hope you will enjoy everything that I will be
+        sharing.
+      </div>
+      <div className="posts"></div>
+    </div>
+  );
 };
 
 export default Blogs;
