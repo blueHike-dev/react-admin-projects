2f55cd99668b05cb6549d1cf09de7aca4c031cfd
simo <wambuisimon712@gmail.com>
2023-11-14 09:58:38 +0300

    morning commit

--- personal-blog/src/components/footer/Footer.jsx
+++ personal-blog/src/components/footer/Footer.jsx
@@ -3,7 +3,14 @@ import "./footer.scss";
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- personal-blog/src/components/projectList/ProjectList.jsx
+++ personal-blog/src/components/projectList/ProjectList.jsx
@@ -4,14 +4,18 @@ import ReadMore from "../readMore/ReadMore";
 const ProjectList = ({ imageUrl, title, desc, link }) => {
   return (
     <div className="card">
-      <img src={imageUrl} alt="" />
-      <h2>{title}</h2>
-      <div className="desc">
-        <ReadMore text={desc} maxLength={40} />
+      <div className="img">
+        <img src={imageUrl} alt="" />
       </div>
-      <a href={link}>
-        <button className="project">View Project</button>
-      </a>
+      <div className="words">
+        <h2>{title}</h2>
+        <div className="desc">
+          <ReadMore text={desc} maxLength={40} />
+        </div>
+        <a href={link}>
+          <button className="project">View Project</button>
+        </a>
+      </div>
     </div>
   );
 };
