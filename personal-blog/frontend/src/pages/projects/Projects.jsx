fddfa902534840719ab6f4627e34be17fc00e4af
simo <wambuisimon712@gmail.com>
2023-11-19 23:23:15 +0300

    first commit

--- personal-blog/frontend/src/pages/projects/Projects.jsx
+++ personal-blog/frontend/src/pages/projects/Projects.jsx
@@ -5,17 +5,22 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="projects">
-      {projectData.map((project) => (
-        <ProjectList
-          key={project.id}
-          imageUrl={project.imageUrl}
-          title={project.title}
-          desc={project.desc}
-          link={project.link}
-        />
-      ))}
-    </div>
+    <>
+      <div className="projects">
+        <div className="project">
+          {projectData.map((project) => (
+            <ProjectList
+              key={project.id}
+              imageUrl={project.imageUrl}
+              title={project.title}
+              desc={project.desc}
+              link={project.link}
+            />
+          ))}
+        </div>
+        <div className="border"></div>
+      </div>
+    </>
   );
 };
 
