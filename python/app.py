61d2aa3e960fa3fbd04656bebd365bca8f791193
simo <wambuisimon712@gmail.com>
2023-11-23 15:09:01 +0300

    first commit

--- python/app.py
+++ python/app.py
@@ -1,1 +1,30 @@
-print('simoblues')
+# name = input('what is your name?')
+# print('Hi' + name)
+
+
+# birth_year = input('Birth year: ')
+# age = 2019 - birth_year
+# print(age)
+
+# course = " python course for beginners"
+# print(course)
+
+# course = '''
+# Hi Simon,
+# Here is our firts email to you!
+# Thank You,
+# The support team
+# ''' 
+
+# print(course)
+# course.upper
+
+is_hot = True
+
+if is_hot:
+  print("it's a hot day")
+  print("enjoy your day")
+else:
+  print("it's a cold day")
+  print("Wear warm clothes")
+print("Enjoy your day")
