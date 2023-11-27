dcbe7091e81a8483f0aedd2f5b9d3ea3386d12d4
simo <wambuisimon712@gmail.com>
2023-11-27 23:51:20 +0300

    first commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -23,4 +23,34 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
-router.post("/login");
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("wrong credentials");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJs.enc.Utf8);
+
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
