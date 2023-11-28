166f91da7a1408a18a4e66bbb6896e28f936da7b
simo <wambuisimon712@gmail.com>
2023-11-28 12:20:11 +0300

    first commit

---
+++ backend/routes/user.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+const router = require("express").Router();
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = Crypto.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByAndUpdated(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
