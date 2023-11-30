8e5eb8c079064d1d29fe67419456a8ba9f793ff2
simo <wambuisimon712@gmail.com>
2023-11-30 16:46:09 +0300

    first commit

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -58,9 +58,38 @@ const Register: React.FC = () => {
     <div className="container">
       <h2>Register</h2>
       <form>
-        {/* Similar input fields as in the login component */}
-        {/* Handle input changes and display errors as needed */}
-        {/* Handle registration button click to trigger the registration process */}
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Email or Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={handleRegistration}
+          type="button">
+          Register
+        </button>
       </form>
     </div>
   );
